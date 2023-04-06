69ed616ee4717613448f9c3ff2d039166b1930c7
sang ah <kr.sangah@gmail.com>
2023-04-06 14:41:59 +0900

    230406

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import BlueButton from '../components/BlueButton';
 import { useDispatch } from 'react-redux';
-import { next } from '../store/modules/mbti';
+import mbti, { init, next } from '../store/modules/mbti';
 
 const Header = styled.p`
   font-size: 2.6em;
@@ -18,7 +18,32 @@ const SubHeader = styled.p`
 `;
 
 export default function Start() {
+  const [counts, setCounts] = useState(0);
   const dispatch = useDispatch();
+
+  async function fetchData() {
+    // mbti 데이터 받아오는 코드
+    const resMbtiData = await fetch('http://localhost:4000/data/getdata');
+    if (resMbtiData.status === 200) {
+      const mbtiData = await resMbtiData.json();
+      if (mbtiData[0]) dispatch(init(mbtiData[0]));
+    } else {
+      console.log(await resMbtiData.json());
+    }
+
+    // 방문자 수 받아오는 코드
+    const resGetCounts = await fetch('http://localhost:4000/data/getCount');
+    if (resGetCounts.status === 200) {
+      const countData = await resGetCounts.json();
+      console.log(countData);
+    } else {
+      console.log(await resGetCounts.json());
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -31,7 +56,8 @@ export default function Start() {
       <SubHeader>
         개발자가 흔히 접하는 상황에 따라서
         <br />
-        MBTI를 알아봅시다!
+        MBTI를 알아봅시다!{'\n\n'}
+        {counts} 명이 참여해주셨습니다.
       </SubHeader>
       <BlueButton text="TEST 시작" clickEvent={() => dispatch(next())} />
     </>
